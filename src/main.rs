4490c2fae738f1947ed9f2232cbd3eacf8e4f5b3
leafriend <leafriend@gmail.com>
2016-03-26 20:19:31 +0900

    Add new_empty_board() funciton & its test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn new_empty_board() -> [[bool; 8]; 8] {
+    return [
+        [false, false, false, false, false, false, false, false,],
+        [false, false, false, false, false, false, false, false,],
+        [false, false, false, false, false, false, false, false,],
+        [false, false, false, false, false, false, false, false,],
+        [false, false, false, false, false, false, false, false,],
+        [false, false, false, false, false, false, false, false,],
+        [false, false, false, false, false, false, false, false,],
+        [false, false, false, false, false, false, false, false,],
+    ]
+}
+
+#[test]
+fn test_new_empty_board() {
+    let board: [[bool; 8]; 8] = new_empty_board();
+    for row in 0..8 {
+        for cell in 0..8 {
+            assert!(!board[row][cell],
+                "Cell value should be false at [{}][{}]", row, cell);
+        }
+    }
+}
