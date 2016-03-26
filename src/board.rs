27ed3b3c6b25676bda9f95fd248fed842b460790
leafriend <leafriend@gmail.com>
2016-03-27 00:54:43 +0900

    Let Board impl Index<>

--- src/board.rs
+++ src/board.rs
@@ -1,3 +1,5 @@
+use std::ops::Index;
+
 pub struct Board {
     cells: [[bool; 8]; 8],
 }
@@ -21,6 +23,14 @@ impl Board {
 
 }
 
+impl Index<usize> for Board {
+    type Output = [bool; 8];
+
+    fn index<'a>(&'a self, index: usize) -> &'a [bool; 8] {
+        &self.cells[index]
+    }
+}
+
 #[test]
 fn test_new() {
     let board  = Board::new();
@@ -30,4 +40,10 @@ fn test_new() {
                 "Cell value should be false at [{}][{}]", row, cell);
         }
     }
+}
+
+#[test]
+fn test_index() {
+    let board  = Board::new();
+    assert_eq!(board[0], [false, false, false, false, false, false, false, false, ]);
 }
